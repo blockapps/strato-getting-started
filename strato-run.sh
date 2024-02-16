651a9af45c76d74d0486366e8b6ae0a62d38b896
nikitamendelbaum <nikitamendelbaum@users.noreply.github.com>
2024-02-16 18:12:57 +0700

    make strato-run.sh executable, update placeholders, add certInfo

--- strato-run.sh
+++ strato-run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-NODE_HOST="localhost" \
-  OAUTH_DISCOVERY_URL="OAUTH_DISCOVER_URL_PLACEHOLDER" \
-  OAUTH_CLIENT_ID="OAUTH_CLIENT_ID_PLACEHOLDER" \
-  OAUTH_CLIENT_SECRET="OAUTH_CLIENT_SECRET_PLACEHOLDER" \
-  SENDGRID_API_KEY="SENDGRID_API_KEY_PLACEHOLDER" \
-  VAULT_URL="https://vault.blockapps.net:8093" \
+# Note: NODE_HOST, OAUTH_DISCOVERY_URL, VAULT_URL and STRIPE_PAYMENT_SERVER_URL will be removed from the script for future STRATO versions.
+
+NODE_HOST="<DOMAIN_NAME>" \
   network='mercata-hydrogen' \
+  certInfo='{"orgName":"BlockApps","orgUnit":"Mercata","commonName":"<COMMON_NAME>"}' \
+  OAUTH_DISCOVERY_URL="https://keycloak.blockapps.net/auth/realms/mercata-testnet2/.well-known/openid-configuration" \
+  OAUTH_CLIENT_ID="<CLIENT_ID>" \
+  OAUTH_CLIENT_SECRET="<CLIENT_SECRET>" \
+  SENDGRID_API_KEY="<SENDGRID_API_KEY>" \
+  VAULT_URL="https://vault.blockapps.net:8093" \
   STRIPE_PAYMENT_SERVER_URL="https://payments.mercata-testnet2.blockapps.net" \
   ./strato
