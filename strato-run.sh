61976a4dffd9866f8d68697ffe5a8f4702b8fb6b
nikitamendelbaum <nikitamendelbaum@users.noreply.github.com>
2025-11-19 21:03:18 +0700

    STRATO Mercata V2 support (#119)
    * add support for custom genesis block mode
    
    * add support for custom genesis block mode
    
    * Support STRATO v15+
    
    * Check for health status of mercata-{backend,ui} containers)
    
    * Check for health status of mercata-{backend,ui} containers in addition to nginx container
    
    * Remove backslash
    
    * Support STRATO v15+
    
    * Support STRATO v15+
    
    ---------
    
    Co-authored-by: Dustin Norwood <dustin@blockapps.net>

--- strato-run.sh
+++ strato-run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-NODE_HOST="<DOMAIN_NAME>" \
-  OAUTH_CLIENT_ID="<CLIENT_ID>" \
-  OAUTH_CLIENT_SECRET="<CLIENT_SECRET>" \
-  ssl=${ssl:-false} \
+sudo \
+  NODE_HOST='example.com' \
+  network='helium' \
+  OAUTH_CLIENT_ID='client_id_here' \
+  OAUTH_CLIENT_SECRET='client_secret_here' \
+  ssl=true \
   ./strato
